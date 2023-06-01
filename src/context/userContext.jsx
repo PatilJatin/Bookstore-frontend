b83eb1a048b7841eef1300924f1e6ba7d579d7fb
PatilJatin <jatinp8390@gmail.com>
2023-06-01 12:48:41 +0530

    UserContext created

---
+++ src/context/userContext.jsx
@@ -0,0 +1,72 @@
+import { createContext } from "react";
+import { useContext, useReducer } from "react";
+import reducer from "../reducers/userReducer";
+import axios from "axios";
+
+const UserContext = createContext();
+
+const initialState = {
+  isAuthenticated: JSON.parse(localStorage.getItem("isAuthenticated")) || false,
+  userProfile: JSON.parse(localStorage.getItem("userProfile")) || {},
+};
+
+const UserProvider = ({ children }) => {
+  const API = "http://localhost:4000/api/v1";
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const setAuthStatusAndUserProfile = (status, userData) => {
+    localStorage.setItem("isAuthenticated", JSON.stringify(status));
+    localStorage.setItem("userProfile", JSON.stringify(userData));
+    dispatch({
+      type: "SET_AUTH_USER",
+      payload: { isAuthenticated: status, userProfile: userData },
+    });
+  };
+
+  const setToken = (token) => {
+    localStorage.setItem("token", token);
+  };
+  const deleteToken = () => {
+    localStorage.removeItem("token");
+  };
+  const getToken = () => {
+    return localStorage.getItem("token");
+  };
+  const checkToken = () => {
+    const token = localStorage.getItem("token");
+    const isTokenPresent = !!token;
+    return isTokenPresent;
+  };
+  const handleLogout = async () => {
+    try {
+      await axios.get(`${API}/logout`);
+      deleteToken();
+      localStorage.removeItem("isAuthenticated");
+      localStorage.removeItem("userProfile");
+      setAuthStatusAndUserProfile(false, {}); // Update state with logged out status
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <UserContext.Provider
+      value={{
+        ...state,
+        setToken,
+        deleteToken,
+        checkToken,
+        getToken,
+        API,
+        setAuthStatusAndUserProfile,
+        handleLogout,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUserContext = () => {
+  return useContext(UserContext);
+};
+
+export { UserProvider, useUserContext };
