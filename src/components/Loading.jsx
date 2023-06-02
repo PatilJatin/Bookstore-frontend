0653fdacf47147b2d308c2d17d856afe2dc6a520
PatilJatin <jatinp8390@gmail.com>
2023-06-02 12:15:08 +0530

    Loader added

---
+++ src/components/Loading.jsx
@@ -0,0 +1,20 @@
+const Loading = () => {
+  return (
+    <>
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -1,14 +1,19 @@
+import React from "react";
 import Filter from "../../components/Filter";
 import ProductList from "../../components/ProductList";
+import { useBookContext } from "../../context/bookContext";
+import Loading from "../../components/Loading";
 
 const Books = () => {
+  const { isLoading } = useBookContext();
+
   return (
     <section className="w-full flex justify-between bg-primary-300">
-      <div className="filter basis-[25%] bg-primary-500 w-full  text-white">
+      <div className="filter basis-[25%] bg-primary-500 w-full text-white">
         <Filter />
       </div>
-      <div className="basis-[74%]  ">
-        <ProductList />
+      <div className="basis-[74%] relative">
+        {isLoading ? <Loading /> : <ProductList />}
       </div>
     </section>
   );
