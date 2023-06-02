ec6819a004a352f30804754b80329a8195cb9074
PatilJatin <jatinp8390@gmail.com>
2023-06-02 12:42:12 +0530

    Add Loader to single product page

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -5,9 +5,10 @@ import ImageSlider from "./ImageSlider";
 import FormatPrice from "../helper/FormatPrice";
 import { useUserContext } from "../context/userContext";
 import AddToCart from "./AddToCart";
+import Loading from "./Loading";
 
 const SingleBook = () => {
-  const { getSingleBook, singleBook } = useBookContext();
+  const { getSingleBook, singleBook, isLoading } = useBookContext();
   const { API } = useUserContext();
   const { id } = useParams();
   const { description, images, name, price, stock } = singleBook;
@@ -17,49 +18,54 @@ const SingleBook = () => {
   }, []);
 
   return (
-    <div className="py-6 bg-primary-400 text-black overflow-x-hidden">
-      <div className="product-container bg-gray-50 shadow-dark-sm rounded-2xl w-[80%] mx-auto flex flex-wrap justify-around py-20">
-        <div className="image-container  basis-[50%] flex justify-center py-10">
-          <ImageSlider images={images} />
-        </div>
-        <div className="product-details basis-[40%] space-y-3">
-          <h2 className="text-heading mb-3.5 text-lg font-bold md:text-xl lg:text-2xl 2xl:text-3xl">
-            {name}
-          </h2>
-          <div className="Product-price font-segoe pl-2 text-sm text-gray-400 line-through md:text-base lg:text-lg xl:text-xl">
-            MRP :
-            <del className="mx-1">
-              <FormatPrice
-                price={
-                  price + (Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000)
-                }
-              />
-            </del>
+    <div className="py-6 bg-primary-400 text-black overflow-x-hidden relative ">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="  product-container bg-gray-50 shadow-dark-sm rounded-2xl w-[80%] mx-auto flex flex-wrap justify-around py-20">
+          <div className="image-container  basis-[50%] flex justify-center py-10">
+            <ImageSlider images={images} />
           </div>
-          <p className="text-heading pr-2 font-bold md:pr-0 text-lg lg:pr-2 2xl:pr-0 ">
-            Deal of the Day:{" "}
-            <span>
-              <FormatPrice price={price} />
-            </span>
-          </p>
-          <p className="text-body text-sm leading-6  lg:text-base lg:leading-8">
-            <p className="text-lg font-medium">Description </p>
-            {description}
-          </p>
-
-          <div className="product-avaliblity">
-            <p
-              className={`${
-                stock > 0 ? "bg-green-600 " : "bg-red-600 "
-              } text-lg font-semibold w-[40%]  text-center rounded-md text-white `}
-            >
-              {stock > 0 ? "Available" : "Not Available"}
+          <div className="product-details basis-[40%] space-y-3">
+            <h2 className="text-heading mb-3.5 text-lg font-bold md:text-xl lg:text-2xl 2xl:text-3xl">
+              {name}
+            </h2>
+            <div className="Product-price font-segoe pl-2 text-sm text-gray-400 line-through md:text-base lg:text-lg xl:text-xl">
+              MRP :
+              <del className="mx-1">
+                <FormatPrice
+                  price={
+                    price +
+                    (Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000)
+                  }
+                />
+              </del>
+            </div>
+            <p className="text-heading pr-2 font-bold md:pr-0 text-lg lg:pr-2 2xl:pr-0 ">
+              Deal of the Day:{" "}
+              <span>
+                <FormatPrice price={price} />
+              </span>
             </p>
+            <p className="text-body text-sm leading-6  lg:text-base lg:leading-8">
+              <p className="text-lg font-medium">Description </p>
+              {description}
+            </p>
+
+            <div className="product-avaliblity">
+              <p
+                className={`${
+                  stock > 0 ? "bg-green-600 " : "bg-red-600 "
+                } text-lg font-semibold w-[40%]  text-center rounded-md text-white `}
+              >
+                {stock > 0 ? "Available" : "Not Available"}
+              </p>
+            </div>
+            <hr className="w-[90%] mx-auto my-3" />
+            {stock > 0 && <AddToCart product={singleBook} />}
           </div>
-          <hr className="w-[90%] mx-auto my-3" />
-          {stock > 0 && <AddToCart product={singleBook} />}
         </div>
-      </div>
+      )}
     </div>
   );
 };
