2c405a12451845716ce22198b38490922999f9dc
PatilJatin <jatinp8390@gmail.com>
2023-06-02 11:14:08 +0530

    production v1 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import Login from "./pages/auth/Login.jsx";
 import Signup from "./pages/auth/Signup";
 import ErrorPage from "./pages/error/ErrorPage";
